cb6bf09ae558d5f903be8244f8f8933ef69166af
Iwo Matecki <iwo@iwomi.pl>
2022-05-13 15:22:00 +0200

    Add Map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import '../../utils/fix-map-icon'
 
+import 'leaflet/dist/leaflet.css';
 import './Map.css'
 
 export const Map = ()=>{
     return(
-        <div className="map"/>
+        <div className="map">
+            <MapContainer center={[51.0706471, 16.9988152]} zoom={20}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> & contributors"
+                />
+                <Marker position={[51.07085, 16.998365]}>
+                    <Popup>
+                        <h2>Kościół św. Trójcy</h2>
+                        <p>Moja parafia</p>
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
     )
 }
