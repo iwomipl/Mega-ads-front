68b16072e09476290136a73b74ae4a9d5126e8ae
Iwo Matecki <iwo@iwomi.pl>
2022-05-21 10:32:08 +0200

    Finish Map and Markers

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Header} from './components/layut/Header';
 import {Map} from './components/Map/Map';
 
 import './App.css';
+import {SearchContext} from './contexts/search.context';
 
 export const App = () => {
+    const [search, setSearch] = useState('');
     return (
         <>
-            <Header/>
-            <Map/>
+            <SearchContext.Provider value={{search, setSearch}}>
+                <Header/>
+                <Map/>
+            </SearchContext.Provider>
         </>
     );
 }
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useContext, useEffect, useState} from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import '../../utils/fix-map-icon'
+import {SearchContext} from "../../contexts/search.context";
+import {SimpleAddEntity} from 'types';
 
 import 'leaflet/dist/leaflet.css';
 import './Map.css'
+import { SingleAdd } from "./SingleAdd";
 
 export const Map = ()=>{
+    const {search}= useContext(SearchContext);
+    const [ads, setAds] = useState<SimpleAddEntity[]>([]);
+
+    useEffect(()=>{
+        (async ()=>{
+            const res = await fetch(`http://localhost:3001/ad/search/${search}`);
+            const data = await res.json();
+
+            setAds(data);
+        })();
+    }, [search])
     return(
         <div className="map">
             <MapContainer center={[51.0706471, 16.9988152]} zoom={20}>
@@ -13,12 +27,16 @@ export const Map = ()=>{
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution="&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> & contributors"
                 />
-                <Marker position={[51.07085, 16.998365]}>
-                    <Popup>
-                        <h2>Kościół św. Trójcy</h2>
-                        <p>Moja parafia</p>
-                    </Popup>
-                </Marker>
+
+                {
+                    ads.map(ad=>(
+                        <Marker key={ad.id} position={[ad.lat, ad.lon]}>
+                            <Popup>
+                                <SingleAdd id={ad.id}/>
+                            </Popup>
+                        </Marker>
+                    ))
+                }
             </MapContainer>
         </div>
     )
